7c2e51557dde16684fb5cae6c628fe3e30963793
= <=>
2026-09-25 20:51:20 +0300

    Add base class for database connections and quwry building

---
+++ EducationSystem/Data/Database.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+using SqlKata.Compilers;
+using SqlKata.Execution;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EducationSystem.Data
+{
+    public class Database
+    {
+        private readonly string _connectionString;
+
+        public Database(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public QueryFactory CreateQueryFactory()
+        {
+            var connection = new SqlConnection(_connectionString);
+            var compiler = new SqlServerCompiler();
+            return new QueryFactory(connection, compiler);
+        }
+    }
+}
